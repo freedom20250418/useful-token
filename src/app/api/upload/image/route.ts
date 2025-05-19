20c7d6b640b439fc6644d67d554474ebcc547744
shixuewen <friendlysxw@163.com>
2025-05-19 18:03:42 +0800

    feat: upload image

--- src/app/api/upload/image/route.ts
+++ src/app/api/upload/image/route.ts
@@ -2,23 +2,53 @@ import { AuthTokenClaims, PrivyClient } from "@privy-io/server-auth";
 import { PRIVY_APP_ID, PRIVY_APP_SECRET } from "@/constants/privy";
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { sendAndPay } from "@/lib/arseeding";
 
 const client = new PrivyClient(PRIVY_APP_ID!, PRIVY_APP_SECRET!);
+const sizeLimit = 2 * 1024 * 1024; // 2MB
 
 export async function POST(req: NextRequest) {
   const headers = new Headers(req.headers);
   const headerAuthToken = headers.get("Authorization")?.replace(/^Bearer /, "");
   const cookieAuthToken = req.cookies.get("privy-token")?.value;
-  const authToken = cookieAuthToken || headerAuthToken;
-  let claims: AuthTokenClaims;
-  try {
-    claims = await client.verifyAuthToken(authToken!);
-  } catch (e: any) {
-    return NextResponse.json({ error: e.message }, { status: 401 });
-  }
+  // const authToken = cookieAuthToken || headerAuthToken;
+  // let claims: AuthTokenClaims;
+  // try {
+  //   claims = await client.verifyAuthToken(authToken!);
+  // } catch (e: any) {
+  //   return NextResponse.json({ error: e.message }, { status: 401 });
+  // }
 
-  const files = req.formData().then((formData) => {
-    const files = formData.getAll("files");
-    return files;
+  const formData = await req.formData();
+  const files = formData.getAll("files") as File[];
+  const filesData = await Promise.all(
+    files.map(async (file) => {
+      const buffer = await file.arrayBuffer();
+      return {
+        name: file.name,
+        type: file.type,
+        size: file.size,
+        buffer: Buffer.from(buffer),
+      };
+    })
+  );
+  console.log("filesData", filesData);
+
+  const filesSend = await Promise.all(
+    filesData.map(async (file) => {
+      const { name, type, size, buffer } = file;
+      const res = await sendAndPay(buffer, {
+        tags: [
+          { name: "Content-Type", value: type },
+          { name: "name", value: name },
+        ],
+      });
+      return res;
+    })
+  );
+
+  console.log("filesSend", filesSend);
+  return NextResponse.json(filesSend, {
+    status: 200,
   });
 }
---
+++ src/lib/arseeding.ts
@@ -0,0 +1,37 @@
+import { genNodeAPI, getTokenTagByEver } from "arseeding-js";
+import type { DataItemCreateOptions } from "arseeding-arbundles";
+
+export type ArseedingInstance = ReturnType<typeof genNodeAPI>;
+
+export const ARSEEDING_WALLET_PRIVATE_KEY =
+  process.env.ARSEEDING_WALLET_PRIVATE_KEY;
+export const ARSEED_URL =
+  process.env.ARSEED_URL || "https://arseed.web3infra.dev";
+
+export const getArseedingInstance = async () => {
+  if (!ARSEEDING_WALLET_PRIVATE_KEY) {
+    throw new Error("ARSEEDING_WALLET_PRIVATE_KEY is not set");
+  }
+  const arseedingInstance = await genNodeAPI(ARSEEDING_WALLET_PRIVATE_KEY);
+  // everPay 支持的 token tag (chainType-symbol-id) , 默认用: ethereum-eth-0x0000000000000000000000000000000000000000
+  const payCurrencyTags = await getTokenTagByEver("eth");
+  return {
+    arseedingInstance,
+    payCurrencyTags,
+  };
+};
+
+export async function sendAndPay(data: Buffer, options: DataItemCreateOptions) {
+  const { arseedingInstance, payCurrencyTags } = await getArseedingInstance();
+  const payCurrencyTag = payCurrencyTags[0];
+  const res = await arseedingInstance.sendAndPay(
+    ARSEED_URL,
+    data,
+    payCurrencyTag,
+    options
+  );
+  const itemId = res.order.itemId;
+  const arseedUrl = `${ARSEED_URL}/${itemId}`;
+  const arUrl = `https://arweave.net/${itemId}`;
+  return { arUrl, arseedUrl, ...res };
+}
